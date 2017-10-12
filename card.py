39d0b8c6e2648f9ae09ea7416ea16f7602ac2c88
DarainS <d29703@gmail.com>
2017-10-12 21:26:34 +0800

    add testAllLevelCards method and refactor so much

--- card.py
+++ card.py
@@ -7,27 +7,23 @@ import cProfile
 class Card():
     
     table={'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'T':10,'J':11,'Q':12,'K':13,'A':14}
-    # 双向字典
-    # reversedTable=bidict(table)
 
     def __init__(self,num,tag):
         self.num=num
         self.tag=tag
 
-    def comparetor(this,that):
-        if this.num>that.num:
-            return 1
-        if this.num<that.num:
-            return -1
-        return 0
+    def __gt__(this,that):
+        return this.num>that.num
 
-    def __str__(self):
-        return str(self.num)+self.tag
-        
+    def __lt__(this,that):
+        return this.num<that.num
+
     def __str__(self):
         return str(self.num)+self.tag
 
     def __eq__(self,that):
+        if type(that)!=Card:
+            return False
         if self.num==that.num and self.tag==that.tag:
             return True
         return False
@@ -42,25 +38,32 @@ class Card():
 
 
 class SevenCard():
+
+    levelTable={1:'高牌',2:'一对',3:'两对',4:'三条',5:'顺子',6:'同花',7:'葫芦',8:'四条',9:'同花顺'}
+
     def __init__(self,arr=[]):
         self.arr=arr
         self.maxValue=0
         self.value=0
+        self.levelText=self.levelTable[1]
+
+    def getCardLevel(self):
+        assert self.value>0
+        self.cardLevel=int(str(self.value)[0])
+        return self.levelTable[self.cardLevel]
 
     def __str__(self):
         res=''
         for card in self.arr:
             res+=card.__str__()+' '
-        # TODO 
-        # res+=self.value
         return res
 
     def fromString(s1,s2=''):
         s1=(s1+s2).strip()
         arr=Card.arrayFromString(s1)
-        assert len(arr)>=5
+        assert len(ls)>=5 and len(ls)<=7
         res=SevenCard()
-        res.arr=sorted(arr,key=lambda x:x.num)
+        res.arr.sort(key=lambda card:card.num)
         
         return res
 
@@ -88,7 +91,7 @@ class SevenCard():
         return tags
 
     # 同花顺-9 同花-6
-    def tryResolveStraightFlushAndFlush(self,cards,tagNum):
+    def tryResolveStraightFlushAndFlush(self,cards,numNum,tagNum):
         tag=None
         for t in tagNum:
             if tagNum[t]>=5:
@@ -100,18 +103,15 @@ class SevenCard():
         for card in ls:
             if card.tag!=tag:
                 ls.remove(card)
-        r=self.testStraight(ls,self.generateNumNum(ls))
 
+        r=self.testStraight(ls,self.generateNumNum(ls),[])
         if r:
             lev,mValue=r
-            self.value=self.caculateValueFromTuple((9,mValue))
-            return self.value
-        
-        self.value=self.caculateValueFromTuple((6,ls[-1].num,ls[-2].num,ls[-3].num,ls[-4].num,ls[-5].num))
-        return self.value
+            return (9,mValue)
+        return(6,ls[-1].num,ls[-2].num,ls[-3].num,ls[-4].num,ls[-5].num)
 
     # 顺子-5
-    def testStraight(self,cards,numNum):
+    def testStraight(self,cards,numNum,tagNum):
         nums={x:0 for x in range(1,15)}
         for card in cards:
             nums[card.num]+=1
@@ -127,14 +127,10 @@ class SevenCard():
                 return (5,i)
         return False
 
-    def tryResolveStraight(self,cards,numNum):
-        r=self.testStraight(cards,numNum)
-        if r:
-            self.value=self.caculateValueFromTuple(r)
-            return self.value
-        return False
+    def tryResolveStraight(self,cards,numNum,tagNum):    
+        return self.testStraight(cards,numNum,tagNum)
 
-    def tryResolveFour(self,cards,numNum):
+    def tryResolveFour(self,cards,numNum,tagNum):
         f=0
         for num in range(14,1,-1):
             if numNum[num]>=4:
@@ -144,11 +140,10 @@ class SevenCard():
             return False
         for num in range(14,1,-1):
             if numNum[num]>=1 and num!=f :
-                self.value=self.caculateValueFromTuple((8,f,num))
-                return self.value
+                return(8,f,num)
 
     # 7
-    def tryResolveFullHouse(self,cards,numNum):
+    def tryResolveFullHouse(self,cards,numNum,tagNum):
         f,h=0,0
         for num in range(14,1,-1):
             if numNum[num]>=3:
@@ -161,12 +156,11 @@ class SevenCard():
                     h=num
 
         if f>0 and h>0:
-            self.value=self.caculateValueFromTuple((7,f,h))
-            return self.value
+            return(7,f,h)
         return False
 
     # 4
-    def tryResolveSet(self,cards,numNum):
+    def tryResolveSet(self,cards,numNum,tagNum):
         f,h1,h2=0,0,0
         for num in range(14,1,-1):
             if numNum[num]>=3:
@@ -182,13 +176,11 @@ class SevenCard():
                     else:
                         h2=num
                         break
-        t=(4,f,h1,h2)
-        self.value=self.caculateValueFromTuple(t)
-        return self.value
+        return(4,f,h1,h2)
 
      # 
     
-    def tryResolvePair(self,cards,numNum):
+    def tryResolvePair(self,cards,numNum,tagNum):
         p1,p2,t=0,0,0
         ticker=[]
         for num in range(14,1,-1):
@@ -208,8 +200,7 @@ class SevenCard():
                 if numNum[num]==1:
                     ticker.append(num)
             tup=(2,p1,ticker[0],ticker[1],ticker[2])
-            self.value=self.caculateValueFromTuple(tup)
-            return self.value
+            return tup
 
         if p1!=0 and p2!=0:
             for num in range(14,1,-1):
@@ -217,65 +208,62 @@ class SevenCard():
                     t=num
                     break
             tup=(3,p1,p2,t)
-            self.value=self.caculateValueFromTuple(tup)
-            return self.value
+            return tup
 
-    def tryResolveHigh(self,cards):
+    def tryResolveHigh(self,cards,numNum,tagNum):
         t=(1,cards[-1].num,cards[-2].num,cards[-3].num,cards[-4].num,cards[-5].num)
-        self.value=self.caculateValueFromTuple(t)
-        return self.value
+        return t
 
-    def caculateValueFromTuple(self,tuple):
-        value=[]
-        v=[0,0,0,0,0,0]
-        for index,num in enumerate(tuple):
-            v[index]=num
+    def caculateValueFromIterator(self,iterator):
+        v=[]
+        v.extend(iterator)
+        toApend=6-len(v)
+        v.extend([0 for  i in range(0,toApend)])
         s='%02d%02d%02d%02d%02d%02d'%(v[0],v[1],v[2],v[3],v[4],v[5])
         val=int(s)
         return val
 
-    def resolveMaxValue(self):
+    def caculateAll(self):
+        self.value=self.resolveMaxValue()
+        self.maxValue=self.value
+        self.levelText=self.getCardLevel()
 
+    resolveMethodList=[tryResolveStraightFlushAndFlush,tryResolveFour,tryResolveFullHouse,tryResolveStraight,tryResolveSet,tryResolvePair,tryResolveHigh]
+
+    def resolveMaxValue(self):
         cards=self.arr
         numNum=self.generateNumNum(cards)
         tagNum=self.generateTagNum(cards)
 
-        if self.tryResolveStraightFlushAndFlush(cards,tagNum):
-            self.maxValue=self.value
-            return self.value
-        if self.tryResolveFour(cards,numNum):
-            self.maxValue=self.value
-            return self.value
-        if self.tryResolveFullHouse(cards,numNum):
-            self.maxValue=self.value
-            return self.value
-        if self.tryResolveStraight(cards,numNum):
-            self.maxValue=self.value
-            return self.value
-        if self.tryResolveSet(cards,numNum):
-            self.maxValue=self.value
-            return self.value
-        if self.tryResolvePair(cards,numNum):
-            self.maxValue=self.value
-            return self.value
-        if self.tryResolveHigh(cards):
-            self.maxValue=self.value
-            return self.value
+        for resolve in self.resolveMethodList:
+            t=resolve(self,cards,numNum,tagNum)
+            if t:
+                self.value=self.caculateValueFromIterator(t)
+                return self.value
 
 
-def test7Card():
-    ls5=['AdKdQdJdTd','4dAd5d2d3d','AdAsAhAcTd','2d2h2s8d8h','KdTd8d2d3d','2d3c4dAd5d','AdAcAh4d6d','AdAcKdKcQd','2d2c3d5d6c','Ad9c4c7dKd']
+def testAllLevelCards():
+    from deck import Deck
     fs=[]
-    for s in ls5:
-        seven=SevenCard.fromString(s+'4c')
-        seven.resolveMaxValue()
-        fs.append(seven)
+    levelSet=set([i for i  in range(1,10)])
+    while len(levelSet)>0:
+        deck=Deck()
+        cards=[]
+        for i in range(0,7):
+            cards.append(deck.dealOne())
+        seven=SevenCard.fromCardArray(cards)
+        seven.caculateAll()
+        if seven.cardLevel in levelSet:
+            fs.append(seven)
+            levelSet.remove(seven.cardLevel)
+
     ls2=sorted(fs,key=lambda seven: seven.value)
     for s in ls2:
-        print(s,s.value)
+        print(s,s.levelText,s.value)
+
 
 def main():
-    test7Card()
+    testAllLevelCards()
 
 if __name__ == '__main__':
     main()
