91c29c22faf2d4295a363cf4c6e42e41ac819c5b
Darain <d29703@gmail.com>
2017-10-18 19:53:17 +0800

    创建 range.py 文件

---
+++ src/range.py
@@ -0,0 +1,24 @@
+# -*- coding=utf-8 -*-
+
+pair=['AA','KK','QQ','JJ','TT','99','88','77','66','55','44','33','22']
+
+Axs=['AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s']
+Axo=['AKo','AQo','AJo','ATo','A9o','A8o','A7o','A6o','A5o','A4o','A3o','A2o']
+
+Kxs=[]
+Kxo=[]
+
+Qxs=[]
+Qxo=[]
+
+sc1=['AKs','KQs','QJs','JTs','T9s','98s','87s','76s','65s','54s','43s','32s']
+
+
+
+sc2=['AQs','KJs','QTs','J9s','T8s','97s','86s','75s','64s','53s','42s']
+
+
+
+r50={
+    'AA','KK','QQ'
+}
