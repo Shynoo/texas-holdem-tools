6f1b721518f6b942318347f6858053ca8b89113d
DarainS <d29703@gmail.com>
2017-10-20 18:57:46 +0800

    Update win_result.py

--- src/win_result.py
+++ src/win_result.py
@@ -6,33 +6,65 @@ from card import Card,SevenCard,HandsCard
 from player import Player
 import random
 
-class WinResult():
+class Board():
 
     def __init__(self):
         self.result=[]
         self.playerNum=2
-        self.players=[]
         self.handsList=[]
+        self.deck=None
+        self.resultValue=None
     
-    def _randomGenerate(self,cardList,toDealNum):
+    def _randomGenerate(cardList,toDealNum=5):
         if toDealNum==0:
             return
         ls=random.sample(cardList,toDealNum)
         return ls
 
     def fromStrings(strlist):
-        result=WinResult()
+        result=Board()
+        hs=set()
         for s in strlist:
             hands=HandsCard.fromString(s)
+            if hands[0] in hs or hands[1] in hs:
+                raise 'Error'
+            hs.add(hands[0])
+            hs.add(hands[1])
             result.handsList.append(hands)
         result.playerNum=len(result.handsList)
         return result
 
-    def generateResultByRandom(self,randomNum=1000):
+    def makeFakeResult(self,toDealNum):
+        sevs=[]
+        for h in self.handsList:
+            showList=Board._randomGenerate(self.deck.inDeck,toDealNum=toDealNum)
+            showList.extend(self.deck.showList)
+            showList.extend(h)
+            sev=Board.caculateValueFromCards(showList)
+            sevs.append(sev)
+        return sevs
+
+    def makeBoardResultModel(sev):
+        res=[]
+        tempelete={
+            'level':sev.level,
+            'name':sev.levelText,
+            'winNum':0,
+            'totalNum':0,
+        }
+        return res
+
+    def caculateValueFromCards(cardList):
+        assert len(cardList)>=5 and len(cardList)<=7
+        temp=SevenCard.fromHands(cardList)
+        temp.caculateAll()
+        return temp
+
+    def generateResultByRandom(self,randomNum=1000,toDealNum=5):
         result={}
         for i in range(0,randomNum):
-            deck=Deck.fromHandsList(self.handsList)
-        
+            Board._randomGenerate(self.deck.inDeck)
 
 
-        
+
+
