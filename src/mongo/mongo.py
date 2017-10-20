f7b729479c736c05862c5f3f86d33a2bbc857d28
DarainS <d29703@gmail.com>
2017-10-20 18:55:43 +0800

    Update mongo.py

--- src/mongo/mongo.py
+++ src/mongo/mongo.py
@@ -8,12 +8,9 @@ port=27017
 client=MongoClient(ip,port)
 db=client.poker
 
-ratedb=db['hole_hands_win_rate']
-
-
 dbnameTemplete='card%d_player%d_with_%s_range'
 
-def generateDB(toDealNum=5,playerNum=2,rangee='100%'):
+def generateDB(toDealNum=5,playerNum=2,rangee='169'):
     name=dbnameTemplete%(toDealNum,playerNum,rangee)
     return db[name]
 
@@ -21,6 +18,5 @@ def getSortedData(db,key='winRate',des=-1):
     res=db.find({}).sort([(key,des)])
     return res
 
-c5t9r100db=db['card5_player9_with_100%_range']
-c3t9r100db=db['card3_player9_with_100%_range']
-c4t9r100db=db['card4_player9_with_100%_range']
+c5p2r169db=db['card5_player2_with_169_range']
+]
