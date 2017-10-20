a05e9ca19c776445a1db7b45a990513decf77dbe
DarainS <d29703@gmail.com>
2017-10-20 18:56:15 +0800

    Update data.py

--- src/data.py
+++ src/data.py
@@ -26,62 +26,7 @@ def insertDate(db,dataList):
             ratedb.replace_one({'hands':data['hands']},data,True)
 
 
-def winRateVsAllHands(playerNum=2,toDealNum=None,totalNum=1000):
-    deck=Deck()
-    players=[]
-    for index in range(0,playerNum):
-        p=Player()
-        p.hands.append(deck.dealOne())
-        p.hands.append(deck.dealOne())
-        players.append(p)
-    ls2=players
-    winNum=testWinRate(ls2,totalNum=totalNum,toDealNum=toDealNum)
-    for index,p in enumerate(ls2):
-        print('%s %.1f'%(p.simpleHandsString(),p.winRate*100)+'%',end='  ')
-    print()
-    dataList=[]
-    for index,p in enumerate(players):
-        dataList.append({
-            'hands':ls2[index].simpleHandsString(),
-            'winNum':winNum[index],
-            'totalNum':totalNum,
-        })
-
-    insertDate(mongo.generateDB(toDealNum=toDealNum,playerNum=playerNum,range='100%'),dataList)
-
-
-def handsWinNumForRange(handsRange,totalNum=1000,toDealNum=5):
-    realRange=hands_range.expandRangeToReal(handsRange)
-    for i in range(0,100):
-        dataList=[]
-        hands1=HandsCard.fromString(random.choice(realRange))
-        hands2=HandsCard.fromString(random.choice(realRange))
-        if hands2[0] in hands1 or hands2[1] in hands1:
-            continue
-        handsList=[hands1,hands2]
-        print(hands1,hands2)
-        deck=Deck.fromHandsList([hands1,hands2])
-        winNum=deck.generateWinNum(totalNum=totalNum,toDealNum=5)
-        for index,p in enumerate(handsList):
-            dataList.append({
-                'hands':handsList[index].simpleString(),
-                'winNum':winNum[index],
-                'totalNum':totalNum,
-            })
-    print('insert data begin')
-    insertDate(mongo.generateDB(toDealNum=toDealNum,playerNum=2,rangee=len(handsRange)),dataList)
-
-def autoReduceRange(cur=165,limit=10**7,target=50):
-    while True:
-        db=mongo.generateDB(rangee=str(cur))
-        r=db.find_one({'hands':'AA'})
-        if r['totalNum']>=limit:
-            cur-=5
-        ls2=hands_range.reduceHands(cur)
-        handsWinNumForRange(ls2)
-
-
-def handsWinNumForRange(handsRange,totalNum=1000,toDealNum=5):
+def updateHandsWinNumForRange(handsRange,totalNum=1000,toDealNum=5):
     realRange=hands_range.expandRangeToReal(handsRange)
     print(len(handsRange))
     for i in range(0,totalNum):
@@ -106,7 +51,7 @@ def handsWinNumForRange(handsRange,totalNum=1000,toDealNum=5):
 def topHandsResult(k=0.25):
     if type(k)==float:
         k=int(169*k)
-    ratedb=mongo.ratedb
+    ratedb=mongo.generateDB(rangee='169')
     res=ratedb.find({}).sort([('winRate',-1)])
     handsResult=set()
     for i in range(0,k):
@@ -116,10 +61,17 @@ def topHandsResult(k=0.25):
             print(res[i]['hands'],end=' ')
     return handsResult
 
-def insertDataByRange(handsRange):
-    while True:
-        handsWinNumForRange(handsRange)
 
+def autoReduceRange(cur=160,step=5,limit=300000,target=50):
+    ls2=hands_range.getRangeHands(cur)
+    while True:
+        db=mongo.generateDB(rangee=str(cur))
+        r=db.find_one({'hands':'AA'})
+        if r['totalNum']>=limit:
+            ls2=hands_range.reduceHands(cur)
+            cur-=step
+            continue
+        updateHandsWinNumForRange(ls2)
 
 def main():
     # topHandsResult(k=0.5)
